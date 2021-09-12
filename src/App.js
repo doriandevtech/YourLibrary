7617bb0b48a20b626195732d32f4d140f0198fa8
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2021-09-12 11:35:08 +0200

    First update of the online library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TitreH1 from "./components/Titres/TitreH1";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <TitreH1>Page lisant les Livres</TitreH1>
+        <p>Livres</p>
+        <button>Ajouter</button>
+      </div>
+    );
+  }
 }
 
-export default App;
---
+++ src/components/Titres/TitreH1.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const titreH1 = (props) => (
+    <h1 className="border border-dark p-2 mt-2 text-white text-center bg-primary rounded">{props.children}</h1>
+);
+
+export default titreH1;
+
+import React from "react";
+
+const titreH1 = (props) => (
+    //  Écrire le code ici
+);
+
+export default titreH1;
