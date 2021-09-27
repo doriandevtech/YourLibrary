4fceefb97684f691da4b4974fbf5643dad33e01b
Dorian EMENIR <dorianemenir@gmail.com>
2021-09-27 21:51:15 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import TitreH1 from "./components/Titres/TitreH1";
+import Button from "./components/Buttons/Button";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <TitreH1>Page lisant les Livres</TitreH1>
+        <TitreH1>Page lisant les livres</TitreH1>
         <p>Livres</p>
-        <button>Ajouter</button>
+        <Button texte="AJOUTER" typeBtn="btn-success" clic={() => console.log("Ajout")}></Button>
+        <Button texte="MODIFICATION" typeBtn="btn-warning" clic={() => console.log("Ajout")}></Button>
+        <Button texte="SUPPRESSION" typeBtn="btn-danger" clic={() => console.log("Ajout")}></Button>
       </div>
     );
   }
 }
 
+export default App;
