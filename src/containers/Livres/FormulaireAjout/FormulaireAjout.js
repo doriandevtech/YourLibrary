56ada671c9407d26bdf761b01cf5c95e3b8634ea
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-01-10 22:51:15 +0100

    new src

--- src/containers/Livres/FormulaireAjout/FormulaireAjout.js
+++ src/containers/Livres/FormulaireAjout/FormulaireAjout.js
@@ -1,7 +1,8 @@
 // Import des différentes dépendances requises
 import React, { Component } from 'react';
 import Button from '../../../components/Buttons/Button';
-import {withFormik} from 'formik'
+import {validateYupSchema, withFormik} from 'formik'
+import * as Yup from "yup"
 
 // Component statefull
 class FormulaireAjout extends Component {
@@ -41,7 +42,12 @@ class FormulaireAjout extends Component {
                             name='titre'
                             value={this.props.values.titre}
                             onChange={this.props.handleChange}
+                            onBlur={this.props.handleBlur}
                         />
+                        {
+                            this.props.touched.titre && this.props.errors.titre 
+                            && <span style={{color:"red"}}>{this.props.errors.titre}</span>
+                        }
                     </div>
                     <div className="mb-3">
                         <label htmlFor="auteur" className="form-label">Auteur</label>
@@ -52,7 +58,12 @@ class FormulaireAjout extends Component {
                             name='auteur'
                             value={this.props.values.auteur}
                             onChange={this.props.handleChange}
+                            onBlur={this.props.handleBlur}
                         />
+                        {
+                            this.props.touched.auteur && this.props.errors.auteur 
+                            && <span style={{color:"red"}}>{this.props.errors.auteur}</span>
+                        }
                     </div>
                     <div className="mb-3">
                         <label htmlFor="nbPages" className="form-label">Nombre de pages</label>
@@ -62,8 +73,13 @@ class FormulaireAjout extends Component {
                             id="nbPages"
                             name="nbPages"
                             value={this.props.values.nbPages}
-                            onChange={this.props.handleChange}
+                            onChange={(event) => this.props.setFieldValue('nbPages', +event.target.value)}
+                            onBlur={this.props.handleBlur}
                         />
+                        {
+                            this.props.touched.nbPages && this.props.errors.nbPages 
+                            && <span style={{color:"red"}}>{this.props.errors.nbPages}</span>
+                        }
                     </div>
                     <div className="mb-3">
                         <label htmlFor="nbChapitres" className="form-label">Nombre de chapitres</label>
@@ -73,8 +89,13 @@ class FormulaireAjout extends Component {
                             id="nbChapitres"
                             name="nbChapitres"
                             value={this.props.values.nbChapitres}
-                            onChange={this.props.handleChange}
+                            onChange={(event) => this.props.setFieldValue('nbChapitres', +event.target.value)}
+                            onBlur={this.props.handleBlur}
                         />
+                        {
+                            this.props.touched.nbChapitres && this.props.errors.nbChapitres 
+                            && <span style={{color:"red"}}>{this.props.errors.nbChapitres}</span>
+                        }
                     </div>
                     <div className="mb-3">
                         <label htmlFor="maisonEdition" className="form-label">Maison d'édition</label>
@@ -85,8 +106,14 @@ class FormulaireAjout extends Component {
                             name="maisonEdition"
                             value={this.props.values.maisonEdition}
                             onChange={this.props.handleChange}
+                            onBlur={this.props.handleBlur}
                         />
+                        {
+                            this.props.touched.maisonEdition && this.props.errors.maisonEdition 
+                            && <span style={{color:"red"}}>{this.props.errors.maisonEdition}</span>
+                        }
                     </div>
+                    <Button typeBtn='btn-primary' clic={this.props.handleReset}>Reset</Button>
                     <Button typeBtn='btn-primary' clic={this.props.handleSubmit}>Valider</Button>
                 </form>
             </>
@@ -101,9 +128,46 @@ export default withFormik({
         nbChapitres: '',
         maisonEdition: ''
     }),
-    validate : values => {
-        
-    },
+    // Condition sur les champs du formulaire
+    validationSchema : Yup.object().shape({
+        titre : Yup.string()
+                        .min(3, "Le titre doit avoir plus de 3 caractères") // Minimum 3 caractères pour le titre du livre
+                        .max(15, "Le titre doit avoir moins de 15 caractères") // Maximum 15 caractères pour le titre du livre
+                        .required("Le titre est obligatoire"),
+        auteur : Yup.string()
+                        .min(3, "L'auteur doit avoir plus de 3 caractères") // Minimum 3 caractères pour l'auteur du livre
+                        .max(15, "L'auteur doit avoir moins de 15 caractères") // Maximum 15 caractères pour l'auteur du livre
+                        .required("L'auteur est obligatoire"),
+        nbPages : Yup.number()
+                        .lessThan(1000, "Le nombre de pages doit être infèrieur à 1000")
+                        .moreThan(50, "Le nombre de pages doit être supèrieur à 50")
+                        .required("L'auteur est obligatoire"),
+        nbChapitres : Yup.number()
+                        .lessThan(1000, "Le nombre de chapitres doit être infèrieur à 1000")
+                        .moreThan(50, "Le nombre de chapitres doit être supèrieur à 50")
+                        .required("L'auteur est obligatoire"),
+
+
+    }),
+    // validate : values => {
+        // const errors = {};
+        // if (values.titre.length < 3) {
+        //     errors.titre = "Le titre doit avoir plus de 3 caractères";
+        // }
+        // if (values.titre.length > 15) {
+        //     errors.titre = "Le titre doit avoir moins de 15 caractères"
+        // }
+        // if (!values.auteur) {
+        //     errors.auteur = "Le champ \"Auteur\" est requis"
+        // }
+        // if (!values.nbPages) {
+        //     errors.nbPages = "Le champ \"Nombre de pages\" est requis"
+        // }
+        // if (!values.nbChapitres) {
+        //     errors.auteur = "Le champ \"Nombre de chapitres\" est requis"
+        // }
+        // return errors;
+    // },
     handleSubmit : (values,{props}) => {
         props.validation(values.titre, values.auteur, values.nbPages, values.nbChapitres, values.maisonEdition)
     }
